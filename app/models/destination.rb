76f4de23254095b101ff9bed83cf0fe559ac6653
raigakun <masashiyoshida2017@gmail.com>
2020-03-09 15:04:21 +0900

    destinationテーブルのvalidation設定

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -5,23 +5,16 @@ class Destination < ApplicationRecord
   validates :destination_first_name, presence: true,format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: 'は日本語で入力して下さい。'}
   validates :destination_family_name_kana, presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: 'は全角カタカナで入力して下さい。'}
   validates :destination_first_name_kana, presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: '全角カタカナで入力して下さい。'}
-  validates :post_code, presence: true,format: {with: /^\d{7}$/, message: 'は7桁、ハイフンなしで入力してください'}
+  validates :post_code, presence: true,
+  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'は7桁、ハイフンなしで入力してください'}
   validates :prefecture, presence: true
   validates :city, presence: true
   validates :house_number, presence: true
-  validates :building
-  validates :phone_number
+  # validates :building
+  validates :phone_number, format: {with: /A\d{10}$|^\d{11}Z/, message: 'はハイフンなしで入力してください'}
 
-  include JpPrefecture
-  jp_prefecture :prefecture
-  
-  def prefecture_name
-    JpPrefecture::Prefecture.find(code: prefecture).try(:name)
-  end
-  
-  def prefecture_name=(prefecture_name)
-    self.prefecture = JpPrefecture::Prefecture.find(name: prefecture_name).code
-  end
+ 
 
+  
 
 end
