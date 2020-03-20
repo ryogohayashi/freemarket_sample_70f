c8bb278267c20951f06d31ff704dc679b5cb37cd
raigakun <masashiyoshida2017@gmail.com>
2020-03-20 11:40:05 +0900

    コードレビュー（商品出品時にカテゴリがDBに保存されるか検証）

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,7 +1,7 @@
 $(function () {
   // カテゴリーセレクトボックスのオプションを作成
   function appendOption(category) {
-    var html = `<option value="${category.name}" data-category="${category.id}">${category.name}</option>`;
+    var html = `<option value="${category.id}" data-category="${category.id}">${category.name}</option>`;
     return html;
   }
   // 子カテゴリーの表示作成
@@ -19,7 +19,7 @@ $(function () {
   function appendGrandchidrenBox(insertHTML) {
     var grandchildSelectHtml = '';
     grandchildSelectHtml = `<div class='product-details__categories-input__added' id= 'grandchildren_wrapper'>
-                              <select class="lproduct-details__categories-input--select" id="grandchild_category" name="category_id">
+                              <select class="lproduct-details__categories-input--select" id="grandchild_category" name="item[category_id]">
                                 <option value="---" data-category="---">---</option>
                                 ${insertHTML}
                               </select>
