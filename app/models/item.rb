3ce0e5822370e12441d705c960e8f0aa4a6b40b1
bdr86 <y.kai486@gmail.com>
2020-03-21 14:04:27 +0900

    バリデーション、エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :images, dependent: :destroy
-  # belongs_to :category
-  # 未実装の為コメントアウト
+  belongs_to :category
+  # 追加実装の為コメントアウト
   # belongs_to :brand
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage_payer
@@ -11,18 +11,17 @@ class Item < ApplicationRecord
   belongs_to :buyer, class_name: "User"
 
   accepts_nested_attributes_for :images
-  # , allow_destroy: true
 
-  # バリデーション未実装の為コメントアウト
-  # validates :images, presence: { message: 'は投稿してください。'}
-  # validates :name, presence: { message: 'は入力してください。'}
-  # validates :introduction, presence: { message: 'は入力してください。'}
-  # validates :category_id, presence: true, on: :create 
+  validates :images, presence: { message: 'は投稿してください。'}
+  validates :name, presence: { message: 'は入力してください。'}
+  validates :introduction, presence: { message: 'は入力してください。'}
+  validates :category_id, presence: true, on: :create 
+  # 追加実装の為コメントアウト
   # validates :brand, presence: true, allow_blank: true
-  # validates :condition, presence: { message: 'を選択してください。'}
-  # validates :postage_payer, presence: { message: 'を選択してください。'}
-  # validates :prefecture_code, presence: { message: 'は都道府県を選択してください。'}
-  # validates :preparation_day, presence: { message: 'を選択してください。'}
-  # validates :price, numericality: { message: 'は入力してください。'}
+  validates :condition, presence: { message: 'を選択してください。'}
+  validates :postage_payer, presence: { message: 'を選択してください。'}
+  validates :prefecture_code, presence: { message: 'は都道府県を選択してください。'}
+  validates :preparation_day, presence: { message: 'を選択してください。'}
+  validates :price, numericality: { message: 'は入力してください。'}
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,66 @@
 require 'rails_helper'
+describe Item do
+  describe '#create' do
+    # 1. 全てのカラムが存在すれば登録できること
+    it "全てのカラムのvalidationをクリアできれば登録できる" do
+      item = build(:item)
+      expect(item).to be_valid
+    end
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    # 2. nameが空では登録できないこと
+    it "nameが空白だと登録できない" do
+      item = build(:item, name: nil)
+      item.valid?
+      expect(item.errors[:name]).to include("は１文字以上入力してください。")
+    end
+
+    # 3. introductionが空では登録できないこと
+    it "introductionが空だと登録できない " do
+      item = build(:item, introduction: nil)
+      item.valid?
+      expect(item.errors[:introduction]).to include("は１文字以上入力してください。")
+    end
+
+    # 4. categoryが空では登録できないこと
+    it "categoryが空だと登録できない " do
+      user = build(:item, category: nil)
+      user.valid?
+      expect(item.errors[:category]).to include("を選択してください。")
+    end
+
+    # 5. conditionが空では登録できないこと
+    it "conditionが空白だと登録できない" do
+      item = build(:item, condition: nil)
+      item.valid?
+      expect(item.errors[:condition]).to include("を選択してください。")
+    end
+
+    # 6. postage_payerが空では登録できないこと
+    it "postage_payerが空白だと登録できない" do
+      item = build(:item, postage_payer: nil)
+      item.valid?
+      expect(item.errors[:postage_payer]).to include("を選択してください。")
+    end
+
+    # 7. prefecture_codeが空では登録できないこと
+    it "prefecture_codeが空白だと登録できない" do
+      item = build(:item, prefecture_code: nil)
+      item.valid?
+      expect(item.errors[:prefecture_code]).to include("は都道府県を選択してください。")
+    end
+
+    # 8. preparation_dayが空では登録できないこと
+    it "preparation_dayが空白だと登録できない" do
+      item = build(:item, preparation_day: nil)
+      item.valid?
+      expect(item.errors[:preparation_day]).to include("を選択してください。")
+    end
+
+    # 9. priceが空では登録できないこと
+    it "priceが空白だと登録できない" do
+      item = build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price]).to include("は入力してください。")
+    end
+  end
 end
