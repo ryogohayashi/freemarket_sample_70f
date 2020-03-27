fc4e5a6da483402a75b291dc88029f768ec54cae
raigakun <masashiyoshida2017@gmail.com>
2020-03-27 18:18:53 +0900

    9つ表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,8 @@ class TopController < ApplicationController
   def index
     @user = User.where(user: current_user)
     @items = Item.limit(3).order('id DESC')
+    @items1 = Item.limit(3).offset(3).order('id DESC')
+    @items2 = Item.limit(3).offset(6).order('id DESC')
   end
 
   def show
